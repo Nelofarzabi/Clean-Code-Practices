0460c4b9d32ce7af323de3d74ea99188858df503
Nelofar Zabi <nelofarzabi2000@gmail.com>
2025-04-08 10:49:39 +0430

    Move folders to java directory

--- src/main/java/chapter_02_meaningful_names/pronounceable_names/UnpronounceableNamesExample.java
+++ src/main/java/chapter_02_meaningful_names/pronounceable_names/UnpronounceableNamesExample.java
@@ -3,9 +3,9 @@ package chapter_02_meaningful_names.pronounceable_names;
 public class UnpronounceableNamesExample {
     public static void main(String[] args) {
         int kltm = 100;
-        int dbcty = 15;
+        int dbctyzx = 15;
 
-        int zghpr = kltm + dbcty;
+        int zghpr = kltm + dbctyzx;
 
         System.out.println("Result: " + zghpr);
     }
